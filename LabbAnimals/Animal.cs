102ce7336f4da3ee2eb73febbac8ca8f8c16d8fa
FlorentHajdari <florent.hajdari@hotmail.se>
2024-10-31 20:38:05 +0100

    Properties and Methods

---
+++ LabbAnimals/Animal.cs
@@ -0,0 +1,26 @@
+﻿namespace LabbAnimals
+{
+    public class Animal
+    {
+        public string NameOfAnimal;
+        public int age;
+        public string Color;
+        public string Sound;
+        public string Movement;
+
+        public void Presentation()
+        {
+            Console.WriteLine("The following animal is a/an: " + NameOfAnimal);
+        }
+
+        public void Hunting()
+        {
+            Console.WriteLine("Arghhhh.. I need to " + Movement + "so I can get some food!!! ");
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine(Sound + "... ");
+        }
+    }
+}
